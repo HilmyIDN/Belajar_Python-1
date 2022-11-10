7c6abb2e7336a3e6e412f264363042a8e21da98c
HilmyIDN <116239843+HilmyIDN@users.noreply.github.com>
2022-11-10 09:40:06 +0700

    Add files via upload

---
+++ Tugas.py
@@ -0,0 +1,29 @@
+#Soal 1 (Print Say Hello World to Pyhton) 
+print("Hello World to Pyhton")
+
+#Soal 2 (Aritmatika di Python)
+var1 = 100
+var2 = 50
+hasil_tambah = var1 + var2
+hasil_kurang = var1 - var2
+hasil_kali = var1 * var2
+hasil_bagi = var1 // var2
+
+print(hasil_tambah, hasil_kurang, hasil_kali, hasil_bagi)
+
+#Soal 3 (Assign Variable dan Tipe Data Integer, Float)
+a = 1000
+b = 200
+
+integer = a // b
+float = a / b
+
+print (integer)
+print (float)
+
+
+#Soal 4 (Tipe Data)
+usia = "16 tahun"
+hilmy = 'usiaku:'
+
+print("{} {}".format(hilmy, usia))
